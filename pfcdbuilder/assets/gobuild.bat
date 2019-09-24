dc6b6e20412569cb183bbca1cf3aabcdf784dfbf
J Fixby <github@jfixby.com>
2019-09-24 22:28:41 +0200

    drop

--- pfcdbuilder/assets/gobuild.bat
+++
@@ -1,5 +0,0 @@
-cd #TARGET_FOLDER#
-set GO111MODULE=on
-go build
-go clean -testcache
-go test -v ./...
--- run_tests.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-GO=go
-GO111MODULE=on
-
-  $GO version
-  $GO clean -testcache
-  $GO build -v ./...
-  $GO test -v ./...
