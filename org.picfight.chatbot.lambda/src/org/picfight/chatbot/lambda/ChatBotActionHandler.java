ad426b64c96f5ff93effbe3966b5374d2d6cd5d4
J Fixby <github@jfixby.com>
2019-10-11 15:17:46 +0200

    sys settings

--- org.picfight.chatbot.lambda/src/org/picfight/chatbot/lambda/ChatBotActionHandler.java
+++ org.picfight.chatbot.lambda/src/org/picfight/chatbot/lambda/ChatBotActionHandler.java
@@ -1,6 +1,10 @@
 
 package org.picfight.chatbot.lambda;
 
+import com.jfixby.scarabei.api.collections.Map;
+import com.jfixby.scarabei.api.log.L;
+import com.jfixby.scarabei.api.names.ID;
+import com.jfixby.scarabei.api.sys.settings.SystemSettings;
 import com.jfixby.scarabei.red.desktop.ScarabeiDesktop;
 
 public class ChatBotActionHandler {
@@ -12,6 +16,9 @@ public class ChatBotActionHandler {
 	public static ChatBotActionHandler handler;
 
 	public void handleRequest (final ChatRequestResponse requesthandler) {
+		final Map<ID, Object> settings = SystemSettings.listAllSettings();
+		L.d("SystemSettings", settings);
+
 		requesthandler.output = requesthandler.input;
 	}
 
